c67a5f8c8db90e7ba8f3cb9fc2509cd9247f7338
Jordi Masramon <jordi.masramon@gmail.com>
2015-08-28 13:46:54 +0200

    catepillar CountTriangles

--- CountTriangles.py
+++ CountTriangles.py
@@ -21,10 +21,27 @@ def solution(A): # naive solution
                     count += 1
     return count
 
+def fast_solution(A):
+    count = 0
+    n = len(A)
 
+    A.sort()
+    # print A
+
+    for p in xrange(n-2):
+        for q in xrange(p+1, n-1):
+            r = q + 1
+            # print p,q,r,'->',A[p],A[q],A[r]
+            while r < n and A[r] < A[p] + A[q]:
+                # print 'good one'
+                r += 1
+            # print '  ->', q,r
+            count += r - q - 1
+    return count
 
 
 if __name__ == '__main__':
     print 'Start tests..'
-    print solution([10, 2, 5, 1, 8, 12])
     assert solution([10, 2, 5, 1, 8, 12]) == 4
+
+    assert fast_solution([10, 2, 5, 1, 8, 12]) == 4
