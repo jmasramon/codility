1c8619c98adac8f53d4cbc74717dbb08c7c966bc
Jordi Masramon Solans <jordi.masramon@gmail.com>
2016-04-28 18:14:59 +0200

    also one from hackerank

--- PermMissingElem.py
+++ PermMissingElem.py
@@ -1,7 +1,7 @@
 __author__ = 'jmasramon'
 
 # A[N] integers in range [1,(N+1)]
-def solution(A):
+def solution1(A):
     A.sort()
     n = len(A)
     print 'sorted A:', A, 'len:', n
@@ -16,6 +16,26 @@ def solution(A):
         if (A[i+1]-A[i])>1:
             return A[i]+1
 
+def solution2(A): # not working for all values due to misunderstanding of requs
+    n = len(A)
+    if n <= 1: return 1
+    sA = sorted(A)
+    prev = sA[0]
+    if prev != 1: return 1
+    for e in sA[1:]:
+        if not (e - prev) == 1:
+            return prev + 1
+        prev = e
+
+def solution(A):
+    n = len(A)
+    # if n <= 1: return 1
+    total = 0
+    for elem in xrange(1,n+2):
+        total ^= elem
+    for elem in A:
+        total ^= elem
+    return total
 
 # print primesSmallerThanN([2,3,1,5])
 # print primesSmallerThanN([])
--- TryThings.py
+++ TryThings.py
@@ -1,29 +1,11 @@
 __author__ = 'jmasramon'
 
-def solution1(X, Y, D):
-    pos = X
-    res = 0
-    while (pos<Y):
-        pos += D
-        res += 1
-    return res
+def solution(X, A): # A = fallen leaves. Index is time, value is position
+    fallen = [None]*X
+    for ind, elem in A:
+        if fallen[elem-1] == 0:
+            fallen[elem-1] = 1
+            if sum(fallen) == X: return ind
 
-def solution2(x,y,d):
-    pos = x
-    res = 0
-    while (pos % y == pos):
-        pos += d
-        res += 1
-    return res
 
-def solution(x,y,d):
-    dist = y-x
-    jumps =  dist / d
-    return jumps if (dist % d == 0) else jumps + 1
-
-assert solution(10,85,30) == 3
-assert solution(10,10,30) == 0
-assert solution(10,11,30) == 1
-assert solution(10,11,1) == 1
-# assert solution(10,10,30) == 0
-# assert solution(10,10,30) == 0
+assert solution([1,3,1,4,2,3,5,4]) == 6
