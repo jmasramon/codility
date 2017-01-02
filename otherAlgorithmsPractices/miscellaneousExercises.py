1817be47f63c1f7703d207e78001162d4078e47e
Jordi Masramon Solans <jordi.masramon@gmail.com>
2017-01-02 17:22:32 +0100

    ...

--- otherAlgorithmsPractices/miscellaneousExercises.py
+++ otherAlgorithmsPractices/miscellaneousExercises.py
@@ -77,6 +77,100 @@ print perfMissingElemBis([2,3,1,5,6],5)
 print perfMissingElemBis([4,3,1,5],4)
 print perfMissingElemBis([2,3,4,5],4)
 
-def perfMissingElemBBis(A, n):
+def frogJmp(x, y, d):
+	count = 0
+	while True:
+		count += 1
+		x += d
+		if x>=y: 
+			return count
+
+def frogJmpBis(x,y,d):
+	acc = 0
+	dist = y-x
+	if dist%d:
+		acc = 1
+	acc += dist//d
+	return acc
+
+print (frogJmp(10, 85, 30))
+print (frogJmp(10, 70, 30))
+print (frogJmpBis(10, 85, 30))
+print (frogJmpBis(10, 70, 30))
+
+def tapeEquilibrium(A):
+	left_add = A[0]
+	
+	right_add = 0
+	for i in xrange(1,len(A)):
+		right_add += A[i]
+	
+	print 'intial with', (left_add, right_add) 
+	min_diff = abs(left_add - right_add)
+	print 'so far we get:', min_diff
+
+	for i in xrange(1,len(A)-1):
+		left_add += A[i]
+		right_add -= A[i]
+		
+		print 'trying with', (i, A[i], left_add, right_add)
+		min_diff = min(min_diff, abs(left_add - right_add))
+		print 'so far we get:', min_diff
+
+	return min_diff
+
+def tapeEquilibriumBis(A):
+	addition = 0
+	for i in xrange(len(A)):
+		addition += -1*A[i]
+	res = abs(addition)
+	for i in xrange(len(A)-1):
+		addition += 2*A[i]
+		res = min (res, abs(addition))
+	return res
+
+
+print (tapeEquilibrium([3,1,2,4,3]))
+print (tapeEquilibriumBis([3,1,2,4,3]))
+
+def swapToEqualSum(m, A, B):
+	def countElems(A):
+		numberOfElems = [0]*m
+		sum_res = 0
+		for i in xrange(len(A)):
+			numberOfElems[A[i]] += 1
+			sum_res += A[i]
+		return numberOfElems, sum_res
+	numElemsA, sumA =  (countElems(A))
+	numElemsB, sumB =  (countElems(B))
+
+	numNonCommonElems = [0]*m
+	for i in xrange(m):
+		numNonCommonElems[i] = abs(numElemsA[i] - numElemsB[i])
+
+	return (numElemsA, sumA, numElemsB, sumB, numNonCommonElems)
+
+print swapToEqualSum(4, [1,2,3,0,0,2,3,0], [1,2,3,1,0,2,3,0])
+print swapToEqualSum(4, [1,2,3,1,1,2,3,0], [1,2,3,0,0,2,3,0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
